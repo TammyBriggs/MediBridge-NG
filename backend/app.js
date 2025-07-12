5b763860c11eac94c59e4c3ba41141dbe251ead3
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2025-07-12 18:35:38 +0200

    authentication middleware for both frontend and admin/dashboard users

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import fileUpload from "express-fileupload";
 import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
-import appointmentRouter from "./router/appointmentRouter.js";
+// import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
 config({ path: "./config/config.env" });
