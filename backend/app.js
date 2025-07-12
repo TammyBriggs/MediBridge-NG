36cf43ae7d34c5568f42d6f5f795d36db933cfd9
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2025-07-12 15:18:56 +0200

    fixed bug with connecting to environment variables

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,32 @@
 import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+dbConnection();
+
+export default app;
