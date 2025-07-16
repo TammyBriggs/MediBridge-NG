145f763c75a6573c55d334cd0b5d6fb18714e46b
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2025-07-16 15:52:17 +0200

    working on and testing post appoitment route

---
+++ backend/router/appointmentRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {
+  postAppointment
+} from "../controller/appointmentController.js";
+
+const router = express.Router();
+
+router.post("/post", isPatientAuthenticated, postAppointment);
+
+export default router;
